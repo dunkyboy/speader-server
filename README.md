eb26907fbd5c513d6f37f9a1defa93b6a1643c7a
Duncan Armstrong <me@dunkyboy.com>
2016-09-05 02:09:54 -0400

    Added pretty-print option for debugging

--- README.md
+++ README.md
@@ -4,5 +4,7 @@ Simple RSS server with web and JSON API
 ## TODO:
 - Logging
 - Jetty annotations
-- Web (HTML)
+- Better handling of request params
+- Web (HTML) frontend implementation
 - Actually plug it into a MongoDB database!
+- Plug it into actual identity/auth mechanism (OAuth?)
--- src/com/dunkyboy/speader/server/resource/json/JsonFeedsResource.java
+++ src/com/dunkyboy/speader/server/resource/json/JsonFeedsResource.java
@@ -7,6 +7,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -20,25 +21,35 @@ import static java.util.Collections.singletonMap;
 public class JsonFeedsResource extends AbstractJsonResource {
 
     private static final String PARAM_USER = "u";
+    private static final String PARAM_PRETTY = "p";
 
     private final FeedDao feedDao;
     private final ObjectWriter jsonWriter;
+    private final ObjectWriter jsonWriterPretty;
 
     public JsonFeedsResource(FeedDao feedDao) {
         super("feeds");
         this.feedDao = feedDao;
         this.jsonWriter = new ObjectMapper().writer();
+        this.jsonWriterPretty = new ObjectMapper().writerWithDefaultPrettyPrinter();
     }
 
     @Override
     protected Map<String, String> getParameters(HttpServletRequest httpRequest) {
         String user = httpRequest.getParameter(PARAM_USER);
-        return singletonMap(PARAM_USER, user);
+        String pretty = httpRequest.getParameter(PARAM_PRETTY);
+        Map<String, String> params = new HashMap<>();
+        params.put(PARAM_USER, user);
+        params.put(PARAM_PRETTY, pretty);
+        return params;
     }
 
     @Override
     protected String getJson(Map<String, String> params) throws JsonProcessingException {
         List<Feed> feed = feedDao.getFeedsForUser( params.get(PARAM_USER) );
+
+        if ( Boolean.valueOf(params.get(PARAM_PRETTY)) )
+            return jsonWriterPretty.writeValueAsString(feed);
         return jsonWriter.writeValueAsString(feed);
     }
 }
